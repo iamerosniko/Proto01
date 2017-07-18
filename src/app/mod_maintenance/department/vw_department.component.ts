f61060fae1f54a0b3fe4b047dba21d614dbd1e78
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-07-18 11:06:47 -0400

    Maintenance Department
    Working Features:
    1. add Department
    2. edit department

--- src/app/mod_maintenance/department/vw_department.component.ts
+++ src/app/mod_maintenance/department/vw_department.component.ts
@@ -1,16 +1,41 @@
 import { Component } from '@angular/core';
+import { DepartmentSvc } from '../../com_services/department.svc';
+import { Department } from '../../com_entities/entities';
 @Component({
   moduleId: module.id,
   selector: 'vw-dept',
   templateUrl: 'vw_department.component.html',
 })
 export class VWDepartmentComponent {
-  constructor(){
-
+  constructor(private deptSvc:DepartmentSvc){
+    this.getLocations();
   }
   viewMode : number = 0;
-  editDetails(){
+  department : Department = new Department(0,'',true);
+  departments: Department[] = [];
+
+  newDetails(){
+    this.department=new Department(0,'',true);
+  }
+
+  editDetails(dept: Department){
     this.viewMode=1;
     //get detail
+    this.getDetails(dept);
+  }
+  getDetails(dept : Department){
+    this.department = dept;
+  }
+
+  async saveDepartment(){
+    this.viewMode==0 ?  await this.deptSvc.postDepartment(this.department) : await this.deptSvc.putDepartment(this.department);
+    alert("yes");
+    document.getElementById("btnGoBack").click();
+    this.getLocations();
+    this.department=new Department(0,'',true);
+  }
+
+  async getLocations(){
+    this.departments=await this.deptSvc.getDepartments();
   }
 }
