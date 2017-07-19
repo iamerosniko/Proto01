b05c592015a609f605dbc23f400867583e156884
mahikero <federicoparassarmiento@yahoo.com>
2017-07-19 16:23:25 -0400

    update

--- src/app/mod_skillset/profile/profile.component.ts
+++ src/app/mod_skillset/profile/profile.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms'
 @Component({
   selector: 'profile',
   templateUrl: 'profile.component.html',
 })
 export class ProfileComponent {
   testData: string;
+  skillsetForm: FormGroup;
+  skuName: string;
 
-  constructor(){
+  constructor(fb: FormBuilder){
     this.testData = "Rico"
+
+    this.skillsetForm = fb.group({
+      'skuName': ['ft', Validators.required]
+    });
+    
+  }
+
+  onSubmit(form: any): void {
+    console.log('you submitted value:', form);
+    //this.testData = this.skillsetForm.controls.sku.value;
+
   }
 }
--- src/app/mod_skillset/skillset.module.ts
+++ src/app/mod_skillset/skillset.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Ng2Datetime } from 'ng2-datetime-picker';
 import { HttpModule } from '@angular/http';
 //skillset
@@ -11,30 +11,25 @@ import { DepartmentComponent } from './department/department.component';
 import { ProfileComponent } from './profile/profile.component';
 //profile component
 import { CommonCompModule } from '../mod_common/common_comp.module';
-import { TextboxNameComponent } from './profile/textbox-name/textbox-name.component';
-import { TextboxPhoneComponent } from './profile/textbox-phone/textbox-phone.component';
-import { ComboboxLocationComponent } from './profile/combobox-location/combobox-location.component';
-import { ComboboxDepartmentComponent } from './profile/combobox-department/combobox-department.component';
-import { ToggleVpnComponent } from './profile/toggle-vpn/toggle-vpn.component';
-import { TextboxUpdatedComponent } from './profile/textbox-updated/textbox-updated.component';
+
 @NgModule({
   declarations: [
     SkillSetComponent,DepartmentComponent,
-    ProfileComponent,
-    TextboxNameComponent,
-    TextboxPhoneComponent,
-    ComboboxLocationComponent,
-    ComboboxDepartmentComponent,
-    ToggleVpnComponent,
-    TextboxUpdatedComponent
+    ProfileComponent
   ],
   imports: [
-    BrowserModule,HttpModule,CommonModule,FormsModule,
-    CommonCompModule,SkillSetRouting
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    SkillSetRouting,
+    CommonCompModule
   ],
   providers: [],
   exports: [
-    SkillSetComponent,DepartmentComponent,
+    SkillSetComponent,
+    DepartmentComponent,
     ProfileComponent,
   ]
 })
