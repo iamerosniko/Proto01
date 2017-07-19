7e85d00a150ff7524c26b12ca7eb01bb637233d1
mahikero <federicoparassarmiento@yahoo.com>
2017-07-19 16:46:56 -0400

    up

--- src/app/mod_skillset/skillset.component.ts
+++ src/app/mod_skillset/skillset.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { 
+  FormBuilder, 
+  FormGroup, 
+  Validators 
+} from '@angular/forms'
+import { Associate } from '../com_entities/entities';
+
 @Component({
   selector: 'skillset',
   templateUrl: 'skillset.component.html',
 })
+
 export class SkillSetComponent {
   constructor(){
 
