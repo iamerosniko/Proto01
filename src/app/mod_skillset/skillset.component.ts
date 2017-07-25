aca9e37358e00c91f92afe46dc966c809d2cd5d1
mahikero <federicoparassarmiento@yahoo.com>
2017-07-25 15:10:30 -0400

    up

--- src/app/mod_skillset/skillset.component.ts
+++ src/app/mod_skillset/skillset.component.ts
@@ -24,112 +24,115 @@ import { DepartmentSvc } from '../com_services/department.svc';
 })
 
 export class SkillSetComponent {
-  private currentUser: string;
+  private currentUser: any;
   private associate: Associate;
   private associates: Associate[];
   private locations: Location[];
   private departments: Department[];
-  
+  private skillsetFrm: FormGroup;
+  private trueForm: any;
 
-
-  skillsetFrm: FormGroup;
-  acs: Associate;
   dateToday: Date;
-  
   //new Date().toISOString().replace('-', '/').split('T')[0].replace('-', '/');
   constructor(
       private curUserSvc: CurrentUserSvc,
       private assSvc: AssociateSvc,
       private locSvc: LocationSvc,
       private depSvc: DepartmentSvc,
-      fb: FormBuilder){
-      
-    this.skillsetFrm = fb.group({
-      'UserName': [''],
+      private fb: FormBuilder){
+        
+    this.skillsetFrm = this.fb.group({
+      'UserName': [' '],
       'Department': [1],
       'Location': [1],
       'VPN': [false],
-      'PhoneNumber': ['',Validators.maxLength(20)],
-      'UpdatedOn': ['']
+      'PhoneNumber': [' ',Validators.maxLength(20)],
+      'UpdatedOn': [' ']
       //<!--value="{{dateToday | date: 'dd/MM/yyyy'}}"/>-->
     });
+
+    this.dateToday = new Date();
+    this.associate = new Associate(
+      0,
+      ' ',
+      ' ',
+      false,
+      0,
+      0,
+      new Date(),
+      false
+    );
   }
   
   //TEMPLATE: this will get all needed data
-  getDependencies(): void {
-    //this.curUserSvc.getCurrentUser().then(user => this.currentUser = user.UserName);
-    //this.assSvc.getAssociates().then(associates => this.associates = associates);
-    //this.locSvc.getLocations().then(locations => this.locations = locations);
-    //this.depSvc.getDepartments().then(departments => this.departments = departments);
-    
+  async getDependencies() {
+    this.currentUser = await this.curUserSvc.getCurrentUser();
+    this.associates = await this.assSvc.getAssociates();
+    this.locations = await this.locSvc.getLocations();
+    this.departments = await this.depSvc.getDepartments();
   }
  
-  getAssData(): void {
-    this.associate =  this.associates.find(associate => associate.UserName == this.currentUser);
+  //this will get info of current user
+  async getCurrentUserData() {
+    this.associate = await this.associates.find(associate => associate.UserName == this.currentUser.UserName);
+    this.associates = await null;
   }
-  async ngOnInit() {
-    this.getDependencies();
+
+  ngOnInit(): void {
+    this.getDependencies().then(() => {
+      this.getCurrentUserData();
+    });
+    // this.getDependencies();
+    // this.getCurrentUserData();
+    //this.createForm();
     
-    this.currentUser = "bermoy";
-    this.dateToday = new Date();
-    this.acs = new Associate(
-      0,
-      "rty",
-      "",
-      true,
-      2,
-      3,
-      this.dateToday,
-      false
-    );
-
-    this.associates = [
-      {AssociateID: 1,
-      UserName: "bermoy",
-      PhoneNumber: " ",
-      VPN: true,
-      DepartmentID: 1,
-      LocationID: 1,
-      UpdatedOn: this.dateToday,
-      IsActive: true},
-      {AssociateID: 1,
-      UserName: "sarmife",
-      PhoneNumber: " ",
-      VPN: true,
-      DepartmentID: 1,
-      LocationID: 1,
-      UpdatedOn: this.dateToday,
-      IsActive: true},
-      {AssociateID: 1,
-      UserName: "albert",
-      PhoneNumber: " ",
-      VPN: true,
-      DepartmentID: 1,
-      LocationID: 1,
-      UpdatedOn: this.dateToday,
-      IsActive: true}
     
-    ];
-
-  await this.getAssData();
-
-    // this. departments = [
-    //    {id: 1, name: "Admin"},
-    //    {id: 2, name: "Marketing"},
-    //    {id: 3, name: "Sales"}
-    //  ];
-
-    //  this.locations = [
-    //    {id: 1, name: "New York"},
-    //    {id: 2, name: "Los Angeles"},
-    //    {id: 3, name: "Houston"}
-    //  ];
     
+    //this.currentUser = "bermoy";
+    // this.associate = new Associate(
+    //   0,
+    //   "rty",
+    //   "",
+    //   true,
+    //   2,
+    //   3,
+    //   this.dateToday,
+    //   false
+    // );
+
+    // this.associates = [
+    //   {AssociateID: 1,
+    //   UserName: "bermoy",
+    //   PhoneNumber: " ",
+    //   VPN: true,
+    //   DepartmentID: 1,
+    //   LocationID: 1,
+    //   UpdatedOn: this.dateToday,
+    //   IsActive: true},
+    //   {AssociateID: 1,
+    //   UserName: "sarmife",
+    //   PhoneNumber: " ",
+    //   VPN: true,
+    //   DepartmentID: 1,
+    //   LocationID: 1,
+    //   UpdatedOn: this.dateToday,
+    //   IsActive: true},
+    //   {AssociateID: 1,
+    //   UserName: "albert",
+    //   PhoneNumber: " ",
+    //   VPN: true,
+    //   DepartmentID: 1,
+    //   LocationID: 1,
+    //   UpdatedOn: this.dateToday,
+    //   IsActive: true}
+    
+    // ];
 }
 
   onSubmit(form: any): void {
-    console.log('you submitted value:', form);
+    //console.log('you submitted value:', form);
     //this.testData = this.skillsetForm.controls.sku.value;
+    this.trueForm = form;
 
   }
 }
